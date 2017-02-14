9d1d0ed6b9d7719557e82e00cedb5959a17c2102
Kyle Harris <Kyle Harris>
2017-02-14 18:44:56 -0500

    Shows raw html preview from js file

--- script.js
+++ script.js
@@ -7,16 +7,32 @@ var phoneNumberInput = document.getElementById('phone-number')
 var updateProfile = function () {
   var firstName = firstNameInput.value
   var lastName = lastNameInput.value
+  var fullName = firstName + ' ' + lastName
   var description = descriptionInput.value
   var email = emailInput.value
   var phoneNumber = phoneNumberInput.value
   var preview = document.getElementById('preview')
+  var rawHTML = document.getElementById('html-preview')
 
   preview.innerHTML =
-  'Hi my name is ' + firstName + ' ' + lastName + ' ' +
-  description +
-  ' If you\'re interested in a date, you can email me at ' + email +
-  ' or give me a call at ' + phoneNumber
+  '<h1>Hi my name is ' + fullName + '!</h1>' +
+  '<p>' + description + '</p>' +
+  '<p>' +
+  '  If you\'re interested in a date, you can email me at' +
+  '  <a href="mailto:EMAIL_ADDRESS" target="_blank">' + email + '</a>' +
+  '  or give me a call at' +
+  '  <a href="tel:PHONE_NUMBER" target="_blank">' + phoneNumber + '</a>.' +
+  '</p>'
+
+  rawHTML.textContent =
+  '<h1>Hi my name is ' + fullName + '!</h1>' +
+  '<p>' + description + '</p>' +
+  '<p>' +
+  '  If you\'re interested in a date, you can email me at' +
+  '  <a href="mailto:EMAIL_ADDRESS" target="_blank">' + email + '</a>' +
+  '  or give me a call at' +
+  '  <a href="tel:PHONE_NUMBER" target="_blank">' + phoneNumber + '</a>.' +
+  '</p>'
 }
 
 firstNameInput.addEventListener('input', updateProfile)
